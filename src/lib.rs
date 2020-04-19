477d90d8b58c423b2ba1239024fc6bfe4753964e
bidhan-a <bidhan619@gmail.com>
2020-04-19 23:43:41 +0545

    lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
+use daggy::{Dag, WouldCycle};
+use std::iter::once;
+
+pub trait Step {
+    fn name() -> String;
+}
+
+pub trait Source {}
+
+pub trait Sink {}
+
+pub struct Processor {}
+
 pub struct Executor {}
 
-impl Executor {}
+impl Executor {
+    fn new() -> Self {
+        Self {}
+    }
+}
