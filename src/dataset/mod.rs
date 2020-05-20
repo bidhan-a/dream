6857ee9c27d52dcfd6fc87027702dd2789d685ca
bidhan-a <bidhan619@gmail.com>
2020-05-20 22:02:19 +0545

    implement Drop for DataSet

--- src/dataset/mod.rs
+++ src/dataset/mod.rs
@@ -3,55 +3,65 @@ use std::sync::mpsc::{self, Receiver, Sender};
 use std::thread;
 
 pub struct DataSet<T> {
-    input_rx: Receiver<T>,
+    input_rx: Option<Receiver<T>>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl<T: std::marker::Send + 'static> DataSet<T> {
     pub fn new(input_rx: Receiver<T>) -> DataSet<T> {
-        DataSet { input_rx }
+        DataSet {
+            input_rx: Some(input_rx),
+            thread: None,
+        }
     }
 
-    pub fn map<U: 'static, F: 'static>(self, f: F) -> DataSet<U>
+    pub fn map<U: 'static, F: 'static>(&mut self, f: F) -> DataSet<U>
     where
-        F: std::marker::Send + Fn(T) -> U,
+        F: std::marker::Sync + std::marker::Send + Fn(T) -> U,
         Self: std::marker::Sized,
         U: std::marker::Send,
     {
         let (output_tx, output_rx) = mpsc::channel::<U>();
-
-        // TODO: Need a signal channel that tells when to stop looping.
-        thread::spawn(move || {
+        let input_rx = self.input_rx.take().unwrap();
+        let thread = thread::spawn(move || {
             loop {
                 // receive data from input channel.
-                let input = self.input_rx.recv().unwrap();
+                let input = input_rx.recv().unwrap();
                 let output = f(input);
                 if output_tx.send(output).is_err() {
                     break;
                 }
             }
         });
-
+        self.thread = Some(thread);
         DataSet::new(output_rx)
     }
 
-    pub fn filter<F: 'static>(self, f: F) -> DataSet<T>
+    pub fn filter<F: 'static>(&mut self, f: F) -> DataSet<T>
     where
         F: std::marker::Send + Fn(&T) -> bool,
         Self: std::marker::Sized,
     {
         let (output_tx, output_rx) = mpsc::channel::<T>();
-
-        // TODO: Need a signal channel that tells when to stop looping.
-        thread::spawn(move || {
+        let input_rx = self.input_rx.take().unwrap();
+        let thread = thread::spawn(move || {
             loop {
                 // receive data from input channel.
-                let input = self.input_rx.recv().unwrap();
+                let input = input_rx.recv().unwrap();
                 if f(&input) && output_tx.send(input).is_err() {
                     break;
                 }
             }
         });
-
+        self.thread = Some(thread);
         DataSet::new(output_rx)
+    }
+}
+
+impl<T> Drop for DataSet<T> {
+    fn drop(&mut self) {
+        if let Some(thread) = self.thread.take() {
+            thread.join().unwrap();
+        }
     }
 }
