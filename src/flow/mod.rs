2364f6d6354bcfc8a6dfa57348568b4b120cdaae
bidhan-a <bidhan619@gmail.com>
2020-05-31 23:16:42 +0545

    experiments

--- src/flow/mod.rs
+++ src/flow/mod.rs
@@ -1,11 +1,28 @@
 use daggy::Dag;
 
-pub trait Processor {
-    fn name(self) -> String;
+pub struct Processor {
+    name: String,
+    id: String,
 }
 
 pub struct Flow {
-    dag: Dag<Box<dyn Processor>, ()>,
+    dag: Dag<Processor, ()>,
 }
 
 impl Flow {}
+
+/*
+
+
+Processor {
+    id: String,
+    name: String,
+    start_signal: channel,
+    records_in: int,
+    records_out: int,
+    bytes_in: int,
+    bytes_out: int
+}
+
+
+*/
