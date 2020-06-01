df91d68441786cc89b31bca6ce2c67a8d351228a
bidhan-a <bidhan619@gmail.com>
2020-06-01 23:54:27 +0545

    commit

--- src/flow/mod.rs
+++ src/flow/mod.rs
@@ -1,15 +1,27 @@
 use daggy::Dag;
 
 pub struct Processor {
-    name: String,
     id: String,
+    name: String,
+}
+
+impl Processor {
+    pub fn new(id: String, name: String) -> Self {
+        Processor { id, name }
+    }
 }
 
 pub struct Flow {
+    processors: Vec<Processor>,
+    edges: Vec<(String, String)>,
     dag: Dag<Processor, ()>,
 }
 
-impl Flow {}
+impl Flow {
+    pub fn add(mut self, processor: Processor) {
+        self.dag.add_node(processor);
+    }
+}
 
 /*
 
