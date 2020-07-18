0180809ea148d54eb4892c1180abef6de18d72fd
bidhan-a <bidhan619@gmail.com>
2020-07-18 20:51:25 +0545

    add examples to convert data from csv to avro and vice versa

--- examples/avro_to_csv.rs
+++ examples/avro_to_csv.rs
@@ -1,10 +1,10 @@
+use avro_rs::from_value;
 use avro_rs::types::Value;
-use avro_rs::Schema;
-use avro_rs::{from_value, to_value};
 use chrono::Local;
+use csv::StringRecord;
 use dream::dataset::DataSet;
 use dream::environment::Environment;
-use dream::sinks::avro::AvroSink;
+use dream::sinks::csv::CSVSink;
 use dream::sources::avro::AvroSource;
 use serde::{Deserialize, Serialize};
 
@@ -13,8 +13,6 @@ struct Weather {
     station: String,
     time: i64,
     temp: i8,
-    #[serde(default)]
-    is_cold: bool,
 }
 
 fn main() {
@@ -35,37 +33,30 @@ fn main() {
 
     // Source AVRO file: https://github.com/apache/avro/blob/master/share/test/data/weather.avro
 
-    // Add Source.
+    // Add Avro Source.
     let mut dataset: DataSet<Value> = env
         .add_source(AvroSource::new().with_filename("data/weather.avro"))
         .name("Avro Source");
 
-    // Specify if the temperature is "cold" (temp <= 0).
-    let mut with_coldness: DataSet<Value> = dataset
+    // Convert Avro `Value` to CSV `StringRecord`.
+    let mut to_csv_row: DataSet<StringRecord> = dataset
         .map(|val: Value| {
-            let mut weather = from_value::<Weather>(&val).unwrap();
-            if weather.temp <= 0 {
-                weather.is_cold = true;
-            }
-            to_value(weather).unwrap()
+            let weather = from_value::<Weather>(&val).unwrap();
+            let row: StringRecord = StringRecord::from(vec![
+                weather.station,
+                weather.time.to_string(),
+                weather.temp.to_string(),
+            ]);
+            row
         })
-        .name("With Coldness");
+        .name("To CSV Row");
 
-    // Add Avro Sink.
-    let raw_schema = r#"
-        {
-            "type": "record",
-            "name": "weather",
-            "fields": [
-                {"name": "station", "type": "string"},
-                {"name": "time", "type": "long"},
-                {"name": "temp", "type": "int"},
-                {"name": "is_cold", "type": "boolean"}
-            ]
-        }
-    "#;
-    let schema = Schema::parse_str(raw_schema).unwrap();
-    with_coldness.add_sink(AvroSink::new(schema).with_filename("data/result.avro"));
+    // Add CSV Sink.
+    to_csv_row.add_sink(
+        CSVSink::new()
+            .with_filename("data/weather.csv")
+            .with_headers(StringRecord::from(vec!["station", "time", "temp"])),
+    );
 
     // Run the pipeline.
     env.run();
